9e15c5105b21c76392b0f62c162844291ca722d9
SherkeyXD <253294679@qq.com>
2023-05-17 22:15:05 +0800

    fix: 修正生成指令

--- generator.py
+++ generator.py
@@ -10,8 +10,8 @@ def main():
             raise Exception(f"群{i['id']}({i['name']})的加群链接未经脱敏处理，请检查")
     
     groups.sort(key=lambda x: x['id'])
-    with open ("src/groups/README.md", 'w', encoding="UTF-8") as f:
-        f.write("# 总览\n可以在右上角的搜索框处搜索群，点击群号可以直接加群\n| 群名称/群头像 | 主要内容 | 群号 |\n| ---- | ---- | ---- |\n")
+    with open ("./src/groups/README.md", 'w+', encoding="UTF-8") as f:
+        f.write("---\neditLink: false\n---\n# 总览\n可以在右上角的搜索框处搜索群，点击群号可以直接加群\n| 群名称/群头像 | 主要内容 | 群号 |\n| ---- | ---- | ---- |\n")
         for i in groups:
             f.write(f'''| <figure><img src="http://p.qlogo.cn/gh/{i['id']}/{i['id']}/100" height="100" width="100" /><figcaption>{i['name']}</figcaption></figure> | {i['tags']} | [{i['id']}]({i['url']}) |\n''')
 
