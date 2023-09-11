96bb845626492c06f034041898000dcc76491666
SherkeyXD <253294679@qq.com>
2023-09-12 00:47:49 +0800

    feat: 运行时添加日志输出

--- main.py
+++ main.py
@@ -10,30 +10,41 @@ parser.add_argument("-tags", help="群标签")
 parser.add_argument("-url", help="加群链接")
 args = parser.parse_args()
 
+
+def url_correct(url: str) -> str:
+    params = url.split("//")[-1].split("?")[-1].split("&")
+    param_dict = {i.split("=")[0]: i.split("=")[1] for i in params}
+    return f"https://qm.qq.com/cgi-bin/qm/qr?_wv={param_dict['_wv']}&k={param_dict['k']}"
+
+
 def correct():
     with open("groups.json", 'r', encoding="UTF-8") as f:
         groups = json.load(f)['groups']
     for i in groups:
-        params = {j.split('=')[0]: j.split('=')[1] for j in i['url'].split('?')[-1].split('&')}
-        i['url'] = f"https://qm.qq.com/cgi-bin/qm/qr?_wv={params['_wv']}&k={params['k']}"
+        i['url'] = url_correct(i['url'])
     groups.sort(key=lambda x: x['id'])
-    with open ("groups.json", 'w+', encoding="UTF-8") as f:
+    with open("groups.json", 'w+', encoding="UTF-8") as f:
         json.dump({"groups": groups}, f, indent=4, ensure_ascii=False)
+
 
 def build():
     correct()
-    with open ("groups.json", 'r', encoding="UTF-8") as f:
+    with open("groups.json", 'r', encoding="UTF-8") as f:
         groups = json.load(f)['groups']
     if not os.path.exists("./src/groups"):
         os.mkdir("./src/groups")
-    with open ("./src/groups/README.md", 'w+', encoding="UTF-8") as f:
-        f.writelines(["---\nri:qq-fill\neditLink: false\n---\n",
-        "# 总览\n可以在右上角的搜索框处搜索群，电脑端点击群名称可以直接加群\n",
-        "| 群名称/群头像 | 主要内容 | 群号 |\n| ---- | ---- | ---- |\n"])
+    with open("./src/groups/README.md", 'w+', encoding="UTF-8") as f:
+        f.writelines(["---\nri:qq-fill\neditLink: false\n---\n\n",
+                      "# 总览\n\n可以在右上角的搜索框处搜索群，电脑端点击群名称可以直接加群\n\n",
+                      "| 群名称/群头像 | 主要内容 | 群号 |\n| ---- | ---- | ---- |\n"])
         for i in groups:
-            f.write(f'''| <figure><img src="http://p.qlogo.cn/gh/{i['id']}/{i['id']}/100" height="100" width="100" /><figcaption>[{i['name']}]({i['url']})</figcaption></figure> | {i['tags']} | {i['id']} |\n''')
+            f.write(
+                f'''| <figure><img src="http://p.qlogo.cn/gh/{i['id']}/{i['id']}/100" height="100" width="100" /><figcaption>[{i['name']}]({i['url']})</figcaption></figure> | {i['tags']} | {i['id']} |\n''')
 
-def modify(id : str, name : str, tags : str, url : str):
+
+def modify(id: str, name: str, tags: str, url: str):
+    url = url_correct(url)
+    print(f"正在添加：\n群名称：{name}\n群号：{id}\n群标签：{tags}\n加群链接：{url}")
     with open("groups.json", 'r', encoding="UTF-8") as f:
         groups = json.load(f)['groups']
     for i in groups:
@@ -44,8 +55,9 @@ def modify(id : str, name : str, tags : str, url : str):
             break
     else:
         groups.append({"id": id, "name": name, "tags": tags, "url": url})
-    with open ("groups.json", 'w+', encoding="UTF-8") as f:
+    with open("groups.json", 'w+', encoding="UTF-8") as f:
         json.dump({"groups": groups}, f, indent=4, ensure_ascii=False)
+    print("添加成功")
     correct()
 
 
