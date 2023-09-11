00e52852cf05a82a491d59fe5b6c56540e8f6dd0
SherkeyXD <253294679@qq.com>
2023-09-11 23:44:51 +0800

    chore: 移除文档链接

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,7 +1,6 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const Navbar = navbar([
-
   { 
     text: "首页", 
     icon: "home", 
@@ -16,10 +15,5 @@ export const Navbar = navbar([
     text: "添加群信息", 
     icon: "note", 
     link: "/contributing/" 
-  },
-  {
-    text: "V2 文档",
-    icon: "note",
-    link: "https://theme-hope.vuejs.press/zh/",
   },
 ]);
